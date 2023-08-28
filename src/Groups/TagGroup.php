427cd7d418088098380d2c6ffe736f192add88a2
Marc Beinder <marc@marcbeinder.com>
2023-08-27 19:59:14 -0500

    Added PCO Groups

---
+++ src/Groups/TagGroup.php
@@ -0,0 +1,34 @@
+<?php
+namespace EncoreDigitalGroup\PlanningCenter\Groups;
+
+use GuzzleHttp\Psr7\Request;
+
+class TagGroup
+{
+    public function all($PCOClient, $query = [])
+    {
+        $config = $GLOBALS['pcoClientConfig'];
+        $headers = [
+            'Authorization' => $config['authorization'],
+            'X-PCO-API-Version' => $config['calendar']['apiVersion'],
+        ];
+
+       $query = http_build_query($query);
+       $request = new Request('GET', 'groups/v2/tag_groups?' . $query, $headers);
+       return $PCOClient->send($request, $query);
+    }
+
+    public function tag($PCOClient, $id, $tag = '', $query = []): string
+    {
+        $config = $GLOBALS['pcoClientConfig'];
+        $headers = [
+            'Authorization' => $config['authorization'],
+            'X-PCO-API-Version' => $config['calendar']['apiVersion'],
+        ];
+        $query = http_build_query($query);
+        $request = new Request('GET', 'groups/v2/tag_groups/' . $id . '/tags/' . $tag . '?' . $query, $headers);
+        return $PCOClient->send($request);
+    }
+}
+
+
