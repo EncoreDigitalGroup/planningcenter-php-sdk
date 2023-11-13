3dd7ea08fbfea6df7f11beeac10b14a81d4f306b
Marc Beinder <marc@marcbeinder.com>
2023-11-13 14:54:20 -0600

    type hinted Person Props with PHP Doc

--- src/People/Person.php
+++ src/People/Person.php
@@ -8,73 +8,103 @@ use stdClass;
 
 class Person
 {
-    /**
-     * @var $id int
-     * @var $given_name string
-     * @var $first_name string
-     * @var $nickname string
-     * @var $middle_name string
-     * @var $last_name string
-     * @var $birthdate DateTime
-     * @var $anniversary DateTime
-     * @var $gender string
-     * @var $grade int
-     * @var $child bool
-     * @var $graduation_year int
-     * @var $site_administrator bool
-     * @var $accounting_administrator bool
-     * @var $people_permissions string
-     * @var $membership string
-     * @var $inactivated_at DateTime
-     * @var $medical_notes string
-     * @var $mfa_configured bool
-     * @var $created_at DateTime
-     * @var $updated_at DateTime
-     * @var $avatar string
-     * @var $name string
-     * @var $demographic_avatar_url string
-     * @var $directory_status string
-     * @var $passed_background_check bool
-     * @var $can_create_forms bool
-     * @var $can_email_lists bool
-     * @var $school_type string
-     * @var $status string
-     * @var $primary_campus_id int
-     * @var $gender_id int
-     * @var $remote_id int
-     */
+    /** @var $id int */
     public $id;
+
+    /** @var $given_name string */
     public $given_name;
+
+    /** @var $first_name string */
     public $first_name;
+
+    /** @var $nickname string */
     public $nickname;
+
+    /** @var $middle_name string */
     public $middle_name;
+
+    /** @var $last_name string */
     public $last_name;
+
+    /** @var $birthdate DateTime */
     public $birthdate;
+
+    /** @var $anniversary DateTime */
     public $anniversary;
+
+    /** @var $gender string */
     public $gender;
+
+    /** @var $grade int */
     public $grade;
+
+    /** @var $child bool */
     public $child;
+
+    /** @var $graduation_year int */
     public $graduation_year;
+
+    /** @var $site_administrator bool */
     public $site_administrator;
+
+    /** @var $accounting_administrator bool */
     public $accounting_administrator;
+
+    /** @var $people_permissions string */
     public $people_permissions;
+
+    /** @var $membership string */
     public $membership;
+
+    /** @var $inactivated_at DateTime */
     public $inactivated_at;
+
+    /** @var $medical_notes string */
     public $medical_notes;
+
+    /** @var $mfa_configured bool */
     public $mfa_configured;
+
+    /** @var $created_at DateTime */
     public $created_at;
+
+    /** @var $updated_at DateTime */
     public $updated_at;
+
+    /** @var $avatar string */
     public $avatar;
+
+    /** @var $name string */
     public $name;
+
+    /** @var $demographic_avatar_url string */
     public $demographic_avatar_url;
+
+    /** @var $directory_status string */
     public $directory_status;
+
+    /** @var $passed_background_check bool */
     public $passed_background_check;
+
+    /** @var $can_create_forms bool */
     public $can_create_forms;
+
+    /** @var $can_email_lists bool */
     public $can_email_lists;
+
+    /** @var $school_type string */
     public $school_type;
+
+    /** @var $status string */
     public $status;
+
+    /** @var $primary_campus_id int */
     public $primary_campus_id;
+
+    /** @var $gender_id int */
     public $gender_id;
+
+    /** @var $remote_id int */
     public $remote_id;
 
     public static function all($PCOClient, $query = [])
