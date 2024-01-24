95ba70f1f7cc0828517359fda3acc28dba7d93ee
Marc Beinder <marc.beinder@podtrics.com>
2024-01-24 15:19:13 -0600

    Created EventInstance Object Class

---
+++ src/Objects/Calendar/EventInstance.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace EncoreDigitalGroup\PlanningCenter\Objects\Calendar;
+
+use EncoreDigitalGroup\PlanningCenter\Traits\HasPlanningCenterClient;
+use GuzzleHttp\Psr7\Request;
+
+class EventInstance
+{
+    use HasPlanningCenterClient;
+
+    public function all($eventId, $query = [])
+    {
+        $headers = [
+            'Authorization' => $this->config->getAuthorization(),
+            'X-PCO-API-Version' => $this->config->getCalenderApiVersion(),
+        ];
+        $query = http_build_query($query);
+        $request = new Request('GET', 'calendar/v2/events/' . $eventId . '/event_instances/?' . $query, $headers);
+
+        return $this->client->send($request);
+    }
+
+    public function get($eventId, $eventInstanceId, $query = []): string
+    {
+        $headers = [
+            'Authorization' => $this->config->getAuthorization(),
+            'X-PCO-API-Version' => $this->config->getCalenderApiVersion(),
+        ];
+        $query = http_build_query($query);
+        $request = new Request('GET', 'calendar/v2/events/' . $eventId . '/event_instances/' . $eventInstanceId . '?' . $query, $headers);
+
+        return $this->client->send($request);
+    }
+}
