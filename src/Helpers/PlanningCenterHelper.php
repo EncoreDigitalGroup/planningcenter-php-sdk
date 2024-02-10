6a229b060b5e582276b5e5a372ce28ada2b09656
Marc Beinder <marc@marcbeinder.com>
2024-02-10 10:26:39 -0600

    Fixed Typos

--- src/Helpers/PlanningCenterHelper.php
+++ src/Helpers/PlanningCenterHelper.php
@@ -13,6 +13,20 @@ class PlanningCenterHelper
         return false;
     }
 
+    public static function wasNotSuccessful($response): bool
+    {
+        if (!$response->sdk->outcome->success) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static function wasUnsuccessful($res): bool
+    {
+        return self::wasNotSuccessful($res);
+    }
+
     public static function wasRateLimited($response): bool
     {
         if ($response->sdk->outcome->rate_limited) {
@@ -22,12 +36,12 @@ class PlanningCenterHelper
         return false;
     }
 
-    public static function wasNoteRateLimited($response): bool
+    public static function wasNotRateLimited($response): bool
     {
         if (!$response->sdk->outcome->rate_limited) {
-            return false;
+            return true;
         }
 
-        return true;
+        return false;
     }
 }
