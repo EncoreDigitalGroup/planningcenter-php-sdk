e782553a20ff383b4813ac9f660dfc283ca55b4b
Marc Beinder <marc@marcbeinder.com>
2024-02-10 09:57:37 -0600

    Added Rate Limit Property

--- src/PlanningCenterClient.php
+++ src/PlanningCenterClient.php
@@ -21,6 +21,7 @@ class PlanningCenterClient
     public function setConfiguration(ClientConfiguration $config): ClientConfiguration
     {
         $this->config = $config;
+
         return $this->config;
     }
 
@@ -34,9 +35,9 @@ class PlanningCenterClient
         $handler = new CurlHandler;
         $stack = HandlerStack::create($handler);
         $this->client = new GuzzleClient([
-                'base_uri' => 'https://api.planningcenteronline.com',
-                'handler' => $stack,
-            ]);
+            'base_uri' => 'https://api.planningcenteronline.com',
+            'handler' => $stack,
+        ]);
     }
 
     public function getClient(): GuzzleClient
@@ -50,32 +51,41 @@ class PlanningCenterClient
         try {
             $res = $client->sendAsync($request)->wait();
         } catch (ClientException $e) {
-            if ($retry <= $retry_limit)
-            {
+            if ($retry <= $retry_limit) {
                 $i = $retry + 1;
                 $GLOBALS['pcoRetryCount'] = $i - 1;
+
                 return $this->send($request, $query, $retry_limit, $i);
             }
+
             return json_encode($this->processResponse($e->getResponse()));
         }
 
         return json_encode($this->processResponse($res));
 
     }
+
     protected function processResponse($res): array
     {
         $response_body = $res->getBody()->getContents();
         $http_response_code = $res->getStatusCode();
         $http_message = $res->getReasonPhrase();
         $success = false;
+        $rate_limited = false;
+
         if ($http_response_code >= 200 && $http_response_code < 300) {
             $success = true;
+        }
+
+        if ($http_response_code == 429) {
+            $rate_limited = true;
         }
 
         $response = [
             'sdk' => [
                 'outcome' => [
                     'success' => $success ?? false,
+                    'rateLimited' => $rate_limited ?? false,
                     'http' => [
                         'status_code' => $http_response_code ?? null,
                         'message' => $http_message ?? null,
@@ -83,7 +93,7 @@ class PlanningCenterClient
                         'retry_count' => $GLOBALS['pcoRetryCount'] ?? 0,
                     ],
                 ],
-            ]
+            ],
         ];
 
         if ($success) {
@@ -95,6 +105,4 @@ class PlanningCenterClient
 
         return $response;
     }
-
-
 }
