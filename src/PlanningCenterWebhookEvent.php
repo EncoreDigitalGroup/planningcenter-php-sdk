403ff3eb5ae2d1f48e82d05a4a8364012148cf1c
Marc Beinder <marc@marcbeinder.com>
2024-03-28 20:13:53 -0500

    Create PlanningCenterWebhookEvent

---
+++ src/PlanningCenterWebhookEvent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace EncoreDigitalGroup\PlanningCenter;
+
+enum PlanningCenterWebhookEvent: string
+{
+    case PEOPLE_PERSON_UPDATED = 'people.v2.events.person.updates';
+    case GROUPS_GROUP_CREATED = 'groups.v2.events.group.created';
+    case GROUPS_GROUP_UPDATED = 'groups.v2.events.group.updated';
+    case GROUPS_GROUP_DESTROYED = 'groups.v2.events.group.destroyed';
+    case GROUPS_MEMBERSHIP_CREATED = 'groups.v2.events.membership.created';
+    case GROUPS_MEMBERSHIP_UPDATED = 'groups.v2.events.membership.updated';
+    case GROUPS_MEMBERSHIP_DESTROYED = 'groups.v2.events.membership.destroyed';
+}
