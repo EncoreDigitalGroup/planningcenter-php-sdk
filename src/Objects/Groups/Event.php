2acfdfe201aac8e21a70c71499c3b9f8dc640dbf
Marc Beinder <marc@marcbeinder.com>
2024-03-30 08:45:42 -0500

    Created Groups/Event Class

---
+++ src/Objects/Groups/Event.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace EncoreDigitalGroup\PlanningCenter\Objects\Groups;
+
+use EncoreDigitalGroup\PlanningCenter\Objects\SdkObjects\ClientResponse;
+use EncoreDigitalGroup\PlanningCenter\Traits\HasPlanningCenterClient;
+use GuzzleHttp\Psr7\Request;
+
+class Event
+{
+    use HasPlanningCenterClient;
+
+    public int $eventId;
+
+    public function all(array $query = []): ClientResponse
+    {
+        $headers = $this->buildHeaders();
+
+        $query = http_build_query($query);
+
+        $request = new Request('GET', 'groups/v2/events?' . $query, $headers);
+
+        return $this->client->send($request);
+    }
+
+    public function get(array $query = []): ClientResponse
+    {
+        $headers = $this->buildHeaders();
+
+        $query = http_build_query($query);
+
+        $request = new Request('GET', 'groups/v2/events/' . $this->eventId . '?' . $query, $headers);
+
+        return $this->client->send($request);
+    }
+}
