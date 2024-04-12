802627445d6287dcf1f1ae71381c628427420305
Marc Beinder <marc@marcbeinder.com>
2024-04-12 18:56:35 -0500

    Add BaseClientResponse to ClientResponse

--- src/Objects/SdkObjects/ClientResponse.php
+++ src/Objects/SdkObjects/ClientResponse.php
@@ -6,13 +6,44 @@
 
 namespace EncoreDigitalGroup\PlanningCenter\Objects\SdkObjects;
 
-class ClientResponse
+use EncoreDigitalGroup\SdkClientFoundation\HttpStatusCode;
+use EncoreDigitalGroup\SdkClientFoundation\SdkObjects\SdkContainer as BaseSdkContainer;
+use EncoreDigitalGroup\SdkClientFoundation\SdkObjects\ClientResponse as BaseClientResponse;
+
+class ClientResponse extends BaseClientResponse
 {
-    public SdkContainer $sdk;
+    public BaseSdkContainer $sdk;
     public object $pco;
 
-    public function __construct()
+    public function __construct(mixed $clientResponse)
     {
+        $responseBody = $clientResponse->getBody()->getContents();
+        $httpStatusCode = $clientResponse->getStatusCode();
+        $httpMessage = $clientResponse->getReasonPhrase();
+
+        if ($httpStatusCode >= HttpStatusCode::OK && $httpStatusCode < HttpStatusCode::MULTIPLE_CHOICES) {
+            $success = true;
+        }
+
+        if ($httpStatusCode == HttpStatusCode::TOO_MANY_REQUESTS) {
+            $rateLimited = true;
+        }
+
         $this->sdk = new SdkContainer;
+        $this->sdk->outcome->success = $success ?? false;
+        $this->sdk->outcome->rateLimited = $rateLimited ?? false;
+        $this->sdk->outcome->http->statusCode = $httpStatusCode ?? null;
+        $this->sdk->outcome->http->message = $httpMessage ?? null;
+        /** @deprecated use http->service instead */
+        $this->sdk->outcome->http->pco = $responseBody;
+        $this->sdk->outcome->http->service = $responseBody;
+        $this->sdk->outcome->http->attempts = $this->attempts ?? 1;
+
+        if ($success ?? false) {
+            $responseBody = json_decode($responseBody);
+            $this->pco = $responseBody;
+            $this->sdk->page->previous = $responseBody->meta->prev->offset ?? null;
+            $this->sdk->page->next = $responseBody->meta->next->offset ?? null;
+        }
     }
 }
