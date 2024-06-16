68b3eabf403e799071235c1a36dc455d1d99eaff
Marc Beinder <marc@marcbeinder.com>
2024-06-15 21:48:21 -0500

    [Helpers] Allow BaseClientResponse or ClientResponse

--- src/Helpers/PlanningCenterHelper.php
+++ src/Helpers/PlanningCenterHelper.php
@@ -7,10 +7,11 @@
 namespace EncoreDigitalGroup\PlanningCenter\Helpers;
 
 use EncoreDigitalGroup\PlanningCenter\Objects\SdkObjects\ClientResponse;
+use EncoreDigitalGroup\SdkClientFoundation\SdkObjects\ClientResponse as BaseClientResponse;
 
 class PlanningCenterHelper
 {
-    public static function wasSuccessful(ClientResponse $response): bool
+    public static function wasSuccessful(BaseClientResponse|ClientResponse $response): bool
     {
         if ($response->sdk->outcome->success) {
             return true;
@@ -19,7 +20,7 @@ class PlanningCenterHelper
         return false;
     }
 
-    public static function wasNotSuccessful(ClientResponse $response): bool
+    public static function wasNotSuccessful(BaseClientResponse|ClientResponse $response): bool
     {
         if (! $response->sdk->outcome->success) {
             return true;
@@ -28,12 +29,12 @@ class PlanningCenterHelper
         return false;
     }
 
-    public static function wasUnsuccessful(ClientResponse $res): bool
+    public static function wasUnsuccessful(BaseClientResponse|ClientResponse $res): bool
     {
         return self::wasNotSuccessful($res);
     }
 
-    public static function wasRateLimited(ClientResponse $response): bool
+    public static function wasRateLimited(BaseClientResponse|ClientResponse $response): bool
     {
         if ($response->sdk->outcome->rateLimited) {
             return true;
@@ -42,7 +43,7 @@ class PlanningCenterHelper
         return false;
     }
 
-    public static function wasNotRateLimited(ClientResponse $response): bool
+    public static function wasNotRateLimited(BaseClientResponse|ClientResponse $response): bool
     {
         if (! $response->sdk->outcome->rateLimited) {
             return true;
@@ -51,7 +52,7 @@ class PlanningCenterHelper
         return false;
     }
 
-    public static function serviceError(ClientResponse $response): bool
+    public static function serviceError(BaseClientResponse|ClientResponse $response): bool
     {
         if ($response->sdk->outcome->http->statusCode >= 500) {
             return true;
@@ -60,7 +61,7 @@ class PlanningCenterHelper
         return false;
     }
 
-    public static function idealRequestOutcome(ClientResponse $response): bool
+    public static function idealRequestOutcome(BaseClientResponse|ClientResponse $response): bool
     {
         if (self::wasNotSuccessful($response) || self::wasRateLimited($response) || self::serviceError($response)) {
             return false;
