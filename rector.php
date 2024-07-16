8d1d08bdf5c7a88e80afbb612a5140046d1981e9
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2024-07-16 11:14:20 -0500

    Update Config Files

--- rector.php
+++ rector.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Encore Digital Group - Planning Center PHP SDK
  * Copyright (c) 2024. Encore Digital Group
@@ -6,7 +7,20 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\Equal\UseIdenticalOverEqualWithSameTypeRector;
+use Rector\CodeQuality\Rector\FuncCall\CompactToVariablesRector;
+use Rector\CodeQuality\Rector\LogicalAnd\LogicalToBooleanRector;
+use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
+use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
+use Rector\CodingStyle\Rector\Use_\SeparateMultiUseImportsRector;
 use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodRector;
+use Rector\Naming\Rector\Assign\RenameVariableToMatchMethodCallReturnTypeRector;
+use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
+use Rector\Naming\Rector\Foreach_\RenameForeachValueVariableToMatchExprVariableRector;
+use Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector;
+use Rector\Strict\Rector\BooleanNot\BooleanInBooleanNotRuleFixerRector;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 
 return RectorConfig::configure()
@@ -17,4 +31,36 @@ return RectorConfig::configure()
     // ->withPhpSets()
     ->withRules([
         AddVoidReturnTypeWhereNoReturnRector::class,
-    ]);
+    ])
+    ->withSkip([
+        CompactToVariablesRector::class,
+        UseIdenticalOverEqualWithSameTypeRector::class,
+        LogicalToBooleanRector::class,
+        CatchExceptionNameMatchingTypeRector::class,
+        EncapsedStringsToSprintfRector::class,
+        RenameForeachValueVariableToMatchExprVariableRector::class,
+        RenameParamToMatchTypeRector::class,
+        RenameVariableToMatchMethodCallReturnTypeRector::class,
+        SeparateMultiUseImportsRector::class,
+        RemoveExtraParametersRector::class,
+        NewlineAfterStatementRector::class,
+        BooleanInBooleanNotRuleFixerRector::class,
+
+        /**
+         * This should not last long here. It is here for Calendar Tagging Settings not having the applyDefaultSettings Method called while tenancy is being implemented
+         * and settings schemas are being migrated. Once default settings are implemented for Calendar Tags, RemoveUnusedPrivateMethodRector should be removed from
+         * this list.
+         */
+        RemoveUnusedPrivateMethodRector::class,
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        privatization: true,
+        naming: true,
+        instanceOf: true,
+        earlyReturn: true,
+        strictBooleans: true
+    );
