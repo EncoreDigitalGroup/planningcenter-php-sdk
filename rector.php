951d9a5217b4629ac981a66e362fc26a8ba95acf
Marc Beinder <marc.beinder@encoredigitalgroup.com>
2026-02-08 08:00:46 -0600

    Update Tooling

--- rector.php
+++ rector.php
@@ -8,10 +8,8 @@
 declare(strict_types=1);
 
 use PHPGenesis\DevUtilities\Rector\Rector;
-use Rector\Config\RectorConfig;
 
-return RectorConfig::configure()
+return Rector::configure()
     ->withPaths([
-        __DIR__ . '/src',
-    ])
-    ->withRules(Rector::rules());
+        __DIR__ . "/src",
+    ]);
